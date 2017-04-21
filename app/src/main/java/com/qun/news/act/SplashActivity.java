7ba5899a3767f772c1d5dbb0243d174d5bd821ce
Qun <slvt@qq.com>
2017-04-21 11:12:26 +0800

    自定义VideoView，控制视频宽度

--- app/src/main/java/com/qun/news/act/SplashActivity.java
+++ app/src/main/java/com/qun/news/act/SplashActivity.java
@@ -4,13 +4,13 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.VideoView;
 
 import com.qun.news.R;
+import com.qun.news.view.MyVideoView;
 
 public class SplashActivity extends AppCompatActivity {
 
-    private VideoView mVideoView;
+    private MyVideoView mVideoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +20,7 @@ public class SplashActivity extends AppCompatActivity {
     }
 
     private void initView() {
-        mVideoView = (VideoView) findViewById(R.id.videoView);
+        mVideoView = (MyVideoView) findViewById(R.id.videoView);
         //播放视频
 //        mVideoView.setVideoURI(Uri.parse("http://kr.mp4"));
         mVideoView.setVideoURI(Uri.parse("android.resource://" + this.getPackageName() + "/" + R.raw.kr36));
--- app/src/main/java/com/qun/news/view/MyVideoView.java
+++ app/src/main/java/com/qun/news/view/MyVideoView.java
@@ -24,4 +24,14 @@ public class MyVideoView extends VideoView {
     public MyVideoView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         this(context, attrs, defStyleAttr);
     }
+
+    //重写测量方法，让视频按照布局显示
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        int width = getDefaultSize(0, widthMeasureSpec);
+        int height = getDefaultSize(0, heightMeasureSpec);
+        setMeasuredDimension(width, height);
+    }
 }
