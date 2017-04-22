1aa9ea7ade5a6cc51f2e9c6e356852b4ab039f73
Qun <slvt@qq.com>
2017-04-22 15:15:29 +0800

    更新项目资源文件

--- app/src/main/java/com/qun/news/fragment/HomeFragment.java
+++ app/src/main/java/com/qun/news/fragment/HomeFragment.java
@@ -18,7 +18,7 @@ public class HomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.activity_home, null);
+        View view = inflater.inflate(R.layout.frag_home, null);
         return view;
     }
 }
