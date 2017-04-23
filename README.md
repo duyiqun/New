27d737b8233208a5717ebec42617e241a5f772cf
Qun <slvt@qq.com>
2017-04-23 11:07:47 +0800

    获取网络数据

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # News
 ####网易新闻的示例代码
 * 将zhbj放置于Tomcat的webapps目录
-* 修改zhbj中所有的之前的ip地址为本机的ip地址
+* 修改zhbj中所有的之前的ip地址为本地ip地址
+* 测试地址：http://本地ip:8080/zhbj/categories.json
--- app/src/main/java/com/qun/news/Home/NewsCenterPage.java
+++ app/src/main/java/com/qun/news/Home/NewsCenterPage.java
@@ -4,6 +4,16 @@ import android.content.Context;
 import android.view.View;
 import android.widget.TextView;
 
+import com.qun.news.utils.HMAPI;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 /**
  * Created by Qun on 2017/4/22.
  */
@@ -24,6 +34,27 @@ public class NewsCenterPage extends BasePage {
     @Override
     public void initData() {
         System.out.println("获取了新闻中心的数据");
+        getNetData();
+    }
+
+    private void getNetData() {
+        Request request = new Request.Builder().url(HMAPI.NEW_CENTER).build();
+        OkHttpClient okHttpClient = new OkHttpClient();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            //服务器请求失败
+            @Override
+            public void onFailure(Call call, IOException e) {
+                System.out.println(e);
+            }
+
+            //服务器请求成
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String json = response.body().string();
+                System.out.println(json);
+            }
+        });
     }
 
 //    public void initData() {
