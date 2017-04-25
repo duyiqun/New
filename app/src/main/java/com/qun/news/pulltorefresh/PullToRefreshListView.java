a7ef4e54b2e2fcccfe8164568cc55416ff8c8523
Qun <slvt@qq.com>
2017-04-26 00:34:06 +0800

    修复pulltorefresh导包错误

--- app/src/main/java/com/qun/news/pulltorefresh/PullToRefreshListView.java
+++ app/src/main/java/com/qun/news/pulltorefresh/PullToRefreshListView.java
@@ -1,7 +1,6 @@
 package com.qun.news.pulltorefresh;
 
 
-
 import android.R;
 import android.content.Context;
 import android.util.AttributeSet;
@@ -11,71 +10,76 @@ import android.widget.AbsListView.OnScrollListener;
 import android.widget.Adapter;
 import android.widget.ListView;
 
-import com.heima.new_sh04.act.pullToRefresh.ILoadingLayout.State;
+import com.qun.news.pulltorefresh.ILoadingLayout.State;
 
 /**
  * 这个类实现了ListView下拉刷新，上加载更多和滑到底部自动加载
  */
 public class PullToRefreshListView extends PullToRefreshBase<ListView> implements OnScrollListener {
-    
-    /**ListView*/
+
+    /**
+     * ListView
+     */
     private ListView mListView;
-    /**用于滑到底部自动加载的Footer*/
+    /**
+     * 用于滑到底部自动加载的Footer
+     */
     private LoadingLayout mLoadMoreFooterLayout;
-    /**滚动的监听器*/
+    /**
+     * 滚动的监听器
+     */
     private OnScrollListener mScrollListener;
-    
-    
-    
+
+
     /**
      * 构造方法
-     * 
+     *
      * @param context context
      */
     public PullToRefreshListView(Context context) {
         this(context, null);
     }
-    
+
     /**
      * 构造方法
-     * 
+     *
      * @param context context
-     * @param attrs attrs
+     * @param attrs   attrs
      */
     public PullToRefreshListView(Context context, AttributeSet attrs) {
         this(context, attrs, 0);
     }
-    
+
     /**
      * 构造方法
-     * 
-     * @param context context
-     * @param attrs attrs
+     *
+     * @param context  context
+     * @param attrs    attrs
      * @param defStyle defStyle
      */
     public PullToRefreshListView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs);
-        
+
         setPullLoadEnabled(false);
     }
 
     @Override
     protected ListView createRefreshableView(Context context, AttributeSet attrs) {
-        ListView listView = new ListView(context,attrs);
+        ListView listView = new ListView(context, attrs);
         listView.setId(R.id.list);
         mListView = listView;
 //        mListView.setFooterDividersEnabled(true);
-        
+
         android.view.ViewGroup.LayoutParams params = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
         mListView.setLayoutParams(params);
         listView.setOnScrollListener(this);
-        
+
         return listView;
     }
-    
+
     /**
      * 设置是否有更多数据的标志
-     * 
+     *
      * @param hasMoreData true表示还有更多的数据，false表示没有更多数据了
      */
     public void setHasMoreData(boolean hasMoreData) {
@@ -83,7 +87,7 @@ public class PullToRefreshListView extends PullToRefreshBase<ListView> implement
             if (null != mLoadMoreFooterLayout) {
                 mLoadMoreFooterLayout.setState(State.NO_MORE_DATA);
             }
-            
+
             LoadingLayout footerLoadingLayout = getFooterLoadingLayout();
             if (null != footerLoadingLayout) {
                 footerLoadingLayout.setState(State.NO_MORE_DATA);
@@ -93,13 +97,13 @@ public class PullToRefreshListView extends PullToRefreshBase<ListView> implement
 
     /**
      * 设置滑动的监听器
-     * 
+     *
      * @param l 监听器
      */
     public void setOnScrollListener(OnScrollListener l) {
         mScrollListener = l;
     }
-    
+
     @Override
     protected boolean isReadyForPullUp() {
         return isLastItemVisible();
@@ -113,31 +117,31 @@ public class PullToRefreshListView extends PullToRefreshBase<ListView> implement
     @Override
     protected void startLoading() {
         super.startLoading();
-        
+
         if (null != mLoadMoreFooterLayout) {
             mLoadMoreFooterLayout.setState(State.REFRESHING);
         }
     }
-    
+
     @Override
     public void onPullUpRefreshComplete() {
         super.onPullUpRefreshComplete();
-        
+
         if (null != mLoadMoreFooterLayout) {
             mLoadMoreFooterLayout.setState(State.RESET);
         }
     }
-    
+
     @Override
     public void setScrollLoadEnabled(boolean scrollLoadEnabled) {
         super.setScrollLoadEnabled(scrollLoadEnabled);
-        
+
         if (scrollLoadEnabled) {
             // 设置Footer
             if (null == mLoadMoreFooterLayout) {
                 mLoadMoreFooterLayout = new FooterLoadingLayout(getContext());
             }
-            
+
             if (null == mLoadMoreFooterLayout.getParent()) {
                 mListView.addFooterView(mLoadMoreFooterLayout, null, false);
             }
@@ -148,7 +152,7 @@ public class PullToRefreshListView extends PullToRefreshBase<ListView> implement
             }
         }
     }
-    
+
     @Override
     public LoadingLayout getFooterLoadingLayout() {
         if (isScrollLoadEnabled()) {
@@ -161,14 +165,13 @@ public class PullToRefreshListView extends PullToRefreshBase<ListView> implement
     @Override
     public void onScrollStateChanged(AbsListView view, int scrollState) {
         if (isScrollLoadEnabled() && hasMoreData()) {
-            if (scrollState == OnScrollListener.SCROLL_STATE_IDLE 
-                    || scrollState == OnScrollListener.SCROLL_STATE_FLING) {
+            if (scrollState == OnScrollListener.SCROLL_STATE_IDLE || scrollState == OnScrollListener.SCROLL_STATE_FLING) {
                 if (isReadyForPullUp()) {
                     startLoading();
                 }
             }
         }
-        
+
         if (null != mScrollListener) {
             mScrollListener.onScrollStateChanged(view, scrollState);
         }
@@ -180,29 +183,29 @@ public class PullToRefreshListView extends PullToRefreshBase<ListView> implement
             mScrollListener.onScroll(view, firstVisibleItem, visibleItemCount, totalItemCount);
         }
     }
-    
+
     @Override
     protected LoadingLayout createHeaderLoadingLayout(Context context, AttributeSet attrs) {
         return new HeaderLoadingLayout(context);
     }
-    
-    
+
+
     /**
      * 表示是否还有更多数据
-     * 
+     *
      * @return true表示还有更多数据
      */
     private boolean hasMoreData() {
         if ((null != mLoadMoreFooterLayout) && (mLoadMoreFooterLayout.getState() == State.NO_MORE_DATA)) {
             return false;
         }
-        
+
         return true;
     }
-    
+
     /**
      * 判断第一个child是否完全显示出来
-     * 
+     *
      * @return true完全显示出来，否则false
      */
     private boolean isFirstItemVisible() {
@@ -211,7 +214,7 @@ public class PullToRefreshListView extends PullToRefreshBase<ListView> implement
         if (null == adapter || adapter.isEmpty()) {
             return true;
         }
-        
+
         int mostTop = (mListView.getChildCount() > 0) ? mListView.getChildAt(0).getTop() : 0;
         if (mostTop >= 0) {
             return true;
@@ -222,7 +225,7 @@ public class PullToRefreshListView extends PullToRefreshBase<ListView> implement
 
     /**
      * 判断最后一个child是否完全显示出来
-     * 
+     *
      * @return true完全显示出来，否则false
      */
     private boolean isLastItemVisible() {
