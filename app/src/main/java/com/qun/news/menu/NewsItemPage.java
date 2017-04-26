aa38e5e4514f11ac5d384814fea1b976f3843b82
Qun <slvt@qq.com>
2017-04-26 19:01:43 +0800

    下拉刷新和加载更多

--- app/src/main/java/com/qun/news/menu/NewsItemPage.java
+++ app/src/main/java/com/qun/news/menu/NewsItemPage.java
@@ -15,6 +15,7 @@ import com.qun.news.Home.BasePage;
 import com.qun.news.R;
 import com.qun.news.adapter.NewsItemAdapter;
 import com.qun.news.bean.NewsItemBean;
+import com.qun.news.pulltorefresh.PullToRefreshBase;
 import com.qun.news.pulltorefresh.PullToRefreshListView;
 import com.qun.news.utils.DensityUtil;
 import com.qun.news.utils.GsonTools;
@@ -42,43 +43,57 @@ public class NewsItemPage extends BasePage {
     private PullToRefreshListView mLv;
     private NewsItemBean mNewsItemBean;
 
+    private List<NewsItemBean.DataBean.NewsBean> news = new ArrayList<>();//用来记录新闻列表数据集合
+    private boolean isReFresh;
     private Handler mHandler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
+            if (isReFresh) {
+                //动态创建小圆点
+                initDots(mNewsItemBean.getData().getTopnews().size());
 
-            //动态创建小圆点
-            initDots(mNewsItemBean.getData().getTopnews().size());
+                //创建出专门用于显示轮播图效果的控件
+                RollViewPager rollViewPager = new RollViewPager(mContext);
+                rollViewPager.setTitles(mTopNewsTitle, mNewPagerTitles);
+                rollViewPager.setImages(mNewsPagerImages);
+                rollViewPager.setDots(dots);
+                rollViewPager.setOnItemClickListener(new RollViewPager.onItemClickListener() {
+                    @Override
+                    public void onItemClick(int position) {
+                        Toast.makeText(mContext, ("热门新闻被点击了" + position), Toast.LENGTH_SHORT).show();
+                    }
+                });
+                rollViewPager.start();
 
-            //创建出专门用于显示轮播图效果的控件
-            RollViewPager rollViewPager = new RollViewPager(mContext);
-            rollViewPager.setTitles(mTopNewsTitle, mNewPagerTitles);
-            rollViewPager.setImages(mNewsPagerImages);
-            rollViewPager.setDots(dots);
-            rollViewPager.setOnItemClickListener(new RollViewPager.onItemClickListener() {
-                @Override
-                public void onItemClick(int position) {
-                    Toast.makeText(mContext, ("热门新闻被点击了" + position), Toast.LENGTH_SHORT).show();
-                }
-            });
-            rollViewPager.start();
+                mTopNewsViewpager.addView(rollViewPager);
+            }
 
-            mTopNewsViewpager.addView(rollViewPager);
 
             //如果头视图已经添加了就不再重复添加即可
             int headerViewsCount = mLv.getRefreshableView().getHeaderViewsCount();
-            if(headerViewsCount==0){
+            if (headerViewsCount == 0) {
                 mLv.getRefreshableView().addHeaderView(mTopView);
             }
 //            mLv.getRefreshableView().addHeaderView(mTopView);
-            NewsItemAdapter newsItemAdapter = new NewsItemAdapter(mContext, mNewsItemBean.getData().getNews());
-            mLv.getRefreshableView().setAdapter(newsItemAdapter);
+            if (mNewsItemAdapter == null) {
+                mNewsItemAdapter = new NewsItemAdapter(mContext, news);
+                mLv.getRefreshableView().setAdapter(mNewsItemAdapter);
+            } else {
+                mNewsItemAdapter.notifyDataSetChanged();
+            }
+            //刷新完毕后要通知控件刷新完成
+            mLv.onPullUpRefreshComplete();//通过加载更多结束了
+            mLv.onPullDownRefreshComplete();//通知下拉刷新结束了
         }
     };
+
     private LinearLayout mDotsLl;//专门显示小圆点的容器
     private List<ImageView> dots = new ArrayList<>();
     private TextView mTopNewsTitle;//用来显示轮播图的标题
     private LinearLayout mTopNewsViewpager;//用来显示轮播图的容器
+    private NewsItemAdapter mNewsItemAdapter;
+    private String mMoreUrl;//由于json数据中只有一个字段来获取新数据，下拉刷新与加载更多都使用这个地址
 
     private void initDots(int size) {
         mDotsLl.removeAllViews();
@@ -118,20 +133,44 @@ public class NewsItemPage extends BasePage {
         mDotsLl = (LinearLayout) mTopView.findViewById(R.id.dots_ll);
         mTopNewsTitle = (TextView) mTopView.findViewById(R.id.top_news_title);
         mTopNewsViewpager = (LinearLayout) mTopView.findViewById(R.id.top_news_viewpager);
+
+        //设置下拉刷新与加载更多可用
+        mLv.setPullRefreshEnabled(true);
+        mLv.setPullLoadEnabled(true);
+        mLv.setScrollLoadEnabled(false);
+
+        mLv.setOnRefreshListener(new PullToRefreshBase.OnRefreshListener<ListView>() {
+            //下拉刷新操作
+            @Override
+            public void onPullDownToRefresh(PullToRefreshBase<ListView> refreshView) {
+                getNetData(true, mMoreUrl);
+            }
+
+            //加载更多操作
+            @Override
+            public void onPullUpToRefresh(PullToRefreshBase<ListView> refreshView) {
+                getNetData(false, mMoreUrl);
+            }
+        });
         return mLv;
     }
 
     @Override
     public void initData() {
         String json = SpUtil.getString(mContext, HMAPI.BASE_URL + this.mUrl, "");
-        if(!TextUtils.isEmpty(json)){
-            parseJson(json);
+        if (!TextUtils.isEmpty(json)) {
+            parseJson(json, true);
         }
-        getNetData();
+        getNetData(true, this.mUrl);
     }
 
-    private void getNetData() {
-        Request request = new Request.Builder().url(HMAPI.BASE_URL + this.mUrl).build();
+    /**
+     * @param isRefresh 用于判断当前是否是下拉刷新
+     * @param moreUrl
+     */
+    private void getNetData(final boolean isRefresh, String moreUrl) {
+        this.isReFresh = isRefresh;
+        Request request = new Request.Builder().url(HMAPI.BASE_URL + moreUrl).build();
         OkHttpClient okHttpClient = new OkHttpClient();
         Call call = okHttpClient.newCall(request);
         call.enqueue(new Callback() {
@@ -144,7 +183,7 @@ public class NewsItemPage extends BasePage {
             public void onResponse(Call call, Response response) throws IOException {
                 String json = response.body().string();
                 System.out.println(json);
-                parseJson(json);
+                parseJson(json, isRefresh);
             }
         });
     }
@@ -152,21 +191,40 @@ public class NewsItemPage extends BasePage {
     List<String> mNewPagerTitles = new ArrayList<>();
     List<String> mNewsPagerImages = new ArrayList<>();
 
-    private void parseJson(String json) {
+    /**
+     * @param json
+     * @param isRefresh 用于判断当前是否是下拉刷新
+     */
+    private void parseJson(String json, boolean isRefresh) {
 
         SpUtil.saveString(mContext, HMAPI.BASE_URL + this.mUrl, json);
 
 //        isLoad = true;
         mNewsItemBean = GsonTools.changeGsonToBean(json, NewsItemBean.class);
 
-        //封装标题
-        //封装图片
-        mNewPagerTitles.clear();
-        mNewsPagerImages.clear();
-        for (NewsItemBean.DataBean.TopnewsBean topnewsBean : mNewsItemBean.getData().getTopnews()) {
-            mNewPagerTitles.add(topnewsBean.getTitle());
-            mNewsPagerImages.add(topnewsBean.getTopimage());
+        //如果是下拉刷新要对热门新闻数据进行操作，要对新闻数据列表进行更新
+        //如果是加载更多，之前的数据不变，更多的数据显示在后面即可
+
+        if (isRefresh) {//下拉刷新
+            //热门新闻的操作
+            //封装标题
+            //封装图片
+            mNewPagerTitles.clear();
+            mNewsPagerImages.clear();
+            for (NewsItemBean.DataBean.TopnewsBean topnewsBean : mNewsItemBean.getData().getTopnews()) {
+                mNewPagerTitles.add(topnewsBean.getTitle());
+                mNewsPagerImages.add(topnewsBean.getTopimage());
+            }
+            //新闻列表操作(将数据全部刷新为最新的)
+            news.clear();
+            news.addAll(mNewsItemBean.getData().getNews());
+        } else {//加载更多
+            //新闻列表操作(将数据显示在现有的新闻之后)
+            news.addAll(mNewsItemBean.getData().getNews());
         }
+
+        //获取更多的数据接口地址
+        mMoreUrl = mNewsItemBean.getData().getMore();
 
         mHandler.sendEmptyMessage(0);
     }
