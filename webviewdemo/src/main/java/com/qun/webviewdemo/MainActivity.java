1e64a099412645da78b2591da33dfb32f4e633cd
Qun <slvt@qq.com>
2017-04-27 11:20:30 +0800

    WebView中js和Android代码互调

--- webviewdemo/src/main/java/com/qun/webviewdemo/MainActivity.java
+++ webviewdemo/src/main/java/com/qun/webviewdemo/MainActivity.java
@@ -4,6 +4,7 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -30,7 +31,10 @@ public class MainActivity extends AppCompatActivity {
         mSettings.setJavaScriptEnabled(true);
         mSettings.setBuiltInZoomControls(true);// 显示缩放按钮(wap网页不支持)
         mSettings.setUseWideViewPort(true);// 支持双击缩放(wap网页不支持)
-        mWebView.loadUrl("http://www.baidu.com/");
+//        mWebView.loadUrl("http://www.baidu.com/");
+
+        //加载本地assets目录下的网页
+        mWebView.loadUrl("file:///android_asset/demo.html");
 
         //让网页跳转在应用内部进行打开
         mWebView.setWebViewClient(new WebViewClient() {
@@ -61,6 +65,31 @@ public class MainActivity extends AppCompatActivity {
                 System.out.println("title: " + title);
             }
         });
+
+        initData();
+    }
+
+    //js与android代码互调
+    private void initData() {
+        /**
+         * 参数一：接口对象
+         * 参数二：接口对象名称(1.对象名称必修与js对象名称一致)
+         */
+        mWebView.addJavascriptInterface(new JsCallAndroid() {
+            @JavascriptInterface//3.在4.1以上版本一定要加该注解
+            @Override
+            public void onCallback() {
+                Toast.makeText(MainActivity.this, "js代码调用了android代码", Toast.LENGTH_SHORT).show();
+            }
+        }, "demo");
+    }
+
+    public interface JsCallAndroid {
+        void onCallback();//2.接口方法必须与js中方法名与方法参数保持一致
+    }
+
+    public void aCallJ(View view) {
+        mWebView.loadUrl("javascript:wave()");
     }
 
     public void before(View view) {
@@ -81,19 +110,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        if (mWebView.canGoBack()) {
-            mWebView.goBack();
-        } else {
-            if (System.currentTimeMillis() - quitTime < 2000) {
-                finish();
-            } else {
-                Toast.makeText(this, "再点击一次，退出应用", Toast.LENGTH_SHORT).show();
-                quitTime = System.currentTimeMillis();
-            }
-        }
-    }
-
-    public void aCallJ(View view) {
         if (mWebView.canGoBack()) {
             mWebView.goBack();
         } else {
