1e353b9d12c91a02e28e228a709bd32ce26b83f4
Qun <slvt@qq.com>
2017-04-30 19:16:27 +0800

    初始化布局

--- gridlayoutdemo/src/main/java/com/qun/gridlayoutdemo/MainActivity.java
+++ gridlayoutdemo/src/main/java/com/qun/gridlayoutdemo/MainActivity.java
@@ -2,12 +2,26 @@ package com.qun.gridlayoutdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.GridLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private GridLayout mGridLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        mGridLayout = (GridLayout) findViewById(R.id.gridLayout);
+        
+    }
+
+    public void addItem(View view) {
+
     }
 }
