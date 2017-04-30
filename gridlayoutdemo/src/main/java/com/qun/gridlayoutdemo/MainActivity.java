09ee55c840aa1027a58cc2aec0d0955f3c0f4ed8
Qun <slvt@qq.com>
2017-04-30 20:31:30 +0800

    完成gridlayout添加条目

--- gridlayoutdemo/src/main/java/com/qun/gridlayoutdemo/MainActivity.java
+++ gridlayoutdemo/src/main/java/com/qun/gridlayoutdemo/MainActivity.java
@@ -1,13 +1,16 @@
 package com.qun.gridlayoutdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.GridLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private GridLayout mGridLayout;
+    private int index;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +21,23 @@ public class MainActivity extends AppCompatActivity {
 
     private void initView() {
         mGridLayout = (GridLayout) findViewById(R.id.gridLayout);
-        
+
     }
 
     public void addItem(View view) {
-
+        //往mGridLayout添加条目
+        TextView tv = new TextView(MainActivity.this);
+        tv.setText(index + "");
+        tv.setBackgroundResource(R.drawable.sharp_tv_normal);
+        index++;
+//        mGridLayout.addView(tv);
+        int margin = 5;//5个像素
+        GridLayout.LayoutParams layoutParams = new GridLayout.LayoutParams();
+        layoutParams.width = getResources().getDisplayMetrics().widthPixels / 4 - 2 * margin;
+        layoutParams.height = GridLayout.LayoutParams.WRAP_CONTENT;
+        tv.setLayoutParams(layoutParams);
+        layoutParams.setMargins(margin, margin, margin, margin);
+        tv.setGravity(Gravity.CENTER);
+        mGridLayout.addView(tv, 0);
     }
 }
